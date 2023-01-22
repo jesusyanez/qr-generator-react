dd475197e2abc23bfe8a7fd382565304fd4196a7
Jesus <chumchum239@gmail.com>
2023-01-22 07:33:13 -0500

    initial commit

---
+++ src/QRCodeGenerator.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function QRCodeGenerator() {
+  const [url, setUrl] = useState("");
+  const [qrImage, setQrImage] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.get(`http://localhost:3000/qr?url=${url}`, {
+        responseType: "arraybuffer",
+      });
+      const base64 = btoa(
+        new Uint8Array(data).reduce(
+          (data, byte) => data + String.fromCharCode(byte),
+          ""
+        )
+      );
+      setQrImage(`data:image/svg+xml;base64,${base64}`);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          placeholder="Enter URL"
+        />
+        <button type="submit">Generate QR Code</button>
+      </form>
+      {qrImage && <img src={qrImage} alt="QR Code" />}
+      {qrImage && (
+        <div>
+          <a href={`http://localhost:3000/qrdl?url=${url}`}>
+            <h3>Download PNG</h3>
+          </a>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default QRCodeGenerator;
